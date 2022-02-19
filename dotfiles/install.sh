64d64613e79c374e88cea404df613eef7426571d
yobrosoft <yobrosoft@icloud.com>
2022-02-19 13:40:11 +0800

    use correct dotfiles path

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 echo "Installing..."
-ln -sf $HOME/projects/playground/dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf $HOME/projects/playground/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf $HOME/projects/src/playground/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf $HOME/projects/src/playground/dotfiles/vim/.vimrc ~/.vimrc
--- dotfiles/zsh/.zshrc
+++ dotfiles/zsh/.zshrc
@@ -1,4 +1,4 @@
-for DOTFILE in `find /Users/$USER/.dotfiles/zsh/system`
+for DOTFILE in `find ${HOME}/projects/src/playground/dotfiles/zsh/system`
 do
   [ -f $DOTFILE ] && [[ $DOTFILE =~ .*\.zsh$ ]] &&  source $DOTFILE
 done
